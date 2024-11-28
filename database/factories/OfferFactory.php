be3da6038facec6ba2129c6fa6e2ddcb6abdc03c
Inma Gonzalez <inmagonzalez045@gmail.com>
2024-11-28 14:24:19 +0100

    feat: add factory

---
+++ database/factories/OfferFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class OfferFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->title(),
+            'company' => $this->faker->company(),
+            'offerStatus' =>$this->faker->randomElement([
+                'Enviado',      
+                'Visto',        
+                'Entrevista',  
+                'Denegado',
+            ]),
+        ];
+    }
+}
