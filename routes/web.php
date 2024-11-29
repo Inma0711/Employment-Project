7d1f53ffcfe78e59c42824d7ad9f6620ad09abd8
Inma Gonzalez <inmagonzalez045@gmail.com>
2024-11-29 14:14:38 +0100

    feat: add index route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', [OfferController::class, 'index'])->name('home');
+
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
